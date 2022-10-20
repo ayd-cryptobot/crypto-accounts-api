4b5f1b82781eb7ef22f17c7a97b1f311b3ae0238
sagarus2020 <juanpablocq@ufps.edu.co>
2022-10-20 17:55:12 -0500

    updated documentacion and account code

--- src/servicios/Accounts.js
+++ src/servicios/Accounts.js
@@ -43,11 +43,9 @@ endpoints.post('/accounts/create',async(req,res)=> {
     //  rol=JSON.parse(id).rol;
      first_name=id.first_name;
      last_name=id.last_name;
-     email=id.email;
      username=id.username;
      telegram_id=id.telegram_id;
-     password=id.password;
-     rol=id.rol;
+     rol="cliente";
 
 
 
@@ -55,7 +53,7 @@ endpoints.post('/accounts/create',async(req,res)=> {
       if (err) throw err;
       console.log("Connected!");
 
-      var sql = "INSERT INTO accounts (telegram_id,first_name, last_name, email, username,  password, rol) VALUES ('"+telegram_id+"','"+first_name+"','"+ last_name+"','"+ email+"','"+ username+"','" + password+"','"+ rol+"');";
+      var sql = "INSERT INTO accounts (telegram_id,first_name, last_name,  username,  rol) VALUES ('"+telegram_id+"','"+first_name+"','"+ last_name+"','"+ username+"','"+ rol+"');";
       con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
@@ -87,11 +85,10 @@ const id=buff;
 
 
    username=id.username;
-   telegram_id=id.telegram_id;
    password=id.password;
-   rol=id.rol;
 
-if(rol=="admin"){
+
+
 
 
    con.connect(function(err) {
@@ -117,9 +114,6 @@ if(rol=="admin"){
     });
   });
   res.end;
-}
-else
-console.log("acceso no autorizado");
 
 })
 endpoints.post('/accounts/edit',async(req,res)=> {
@@ -144,8 +138,7 @@ const id=buff;
    email=id.email;
    username=id.username;
    telegram_id=id.telegram_id;
-   password=id.password;
-   rol=id.rol;
+
 
    
 
@@ -154,7 +147,7 @@ const id=buff;
     if (err) throw err;
    
 
-    var sql = "UPDATE accounts SET first_name='"+ first_name+"', last_name='"+ last_name+"', email='"+ email+"', username='"+ username+"', password='"+password+"', rol='"+ rol+"' WHERE(telegram_id='"+telegram_id+"');";
+    var sql = "UPDATE accounts SET first_name='"+ first_name+"', last_name='"+ last_name+"', email='"+ email+"', username='"+ username+"' WHERE(telegram_id='"+telegram_id+"');";
     console.log(sql);
     con.query(sql, function (err, result) {
       if (err) throw err;
@@ -186,7 +179,7 @@ const id=buff;
   //  rol=JSON.parse(id).rol;
 
    telegram_id=id.telegram_id;
-   email=id.email;
+
    password=id.password;
 
    
@@ -196,7 +189,7 @@ const id=buff;
     if (err) throw err;
    
 
-    var sql = "UPDATE accounts SET  password='"+password+"' WHERE(email='"+email+"');";
+    var sql = "UPDATE accounts SET  password='"+password+"' WHERE(telegram_id='"+telegram_id+"');";
     console.log(sql);
     con.query(sql, function (err, result) {
       if (err) throw err;
@@ -209,17 +202,16 @@ const id=buff;
 
 })
 
-endpoints.post('/accounts/delete',async(req,res)=> {
+endpoints.post('/accounts/delete/:telegram_id',async(req,res)=> {
   console.log(req.body,"este es el body")
 
+  let telegram_id  =req.params.telegram_id
 
   //const buff = Buffer.from(req.body.message.data, 'base64');
   //const buff = Buffer.from(req.body.message.data, 'base64');
-  const buff = req.body;
+
   //const id=buff.toString('utf-8')
-const id=buff;
 
-telegram_id=id.telegram_id;
 
 
    con.connect(function(err) {
